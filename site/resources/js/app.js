8820e690b46a43a73a8dd81167657f84072ba865
vongsakorntie <vongsakorntie@gmail.com>
2020-01-23 15:17:25 +0700

    new corousel , mobile fix

--- site/resources/js/app.js
+++ site/resources/js/app.js
@@ -1,1 +1,32 @@
 require('./bootstrap');
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+// $(".owl-carousel").owlCarousel();
+
+$('.owl-carousel').owlCarousel({
+    stagePadding: 100,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    dots: true,
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 3,
+    center: true,
+    responsive:{
+        0:{
+            items:1,
+            stagePadding: 0,
+            margin:0
+        },
+        600 :{
+            items:2 ,
+            stagePadding: 30 ,
+            margin:0
+        },
+        1200:{
+            items:3 ,
+            margin:0
+        }
+    }
+})
--- site/resources/views/test.blade.php
+++ site/resources/views/test.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
@@ -19,10 +19,27 @@
         <div class="slide" id="slide-5">6</div>
         <div class="slide" id="slide-5">7</div>
         </div> --}}
+        <img src="/icon/icon-home.png">
+        <img src="/icon/icon-mice.png">
+        <img src="/icon/icon-m_his.png">
+        <img src="/icon/icon-pro.png">
 
-<img src="/imgs/1.jpg" alt="Avatar">
-
+        <div class="owl-carousel owl-theme">
+          <div class="item"><h4>1</h4></div>
+          <div class="item"><h4>2</h4></div>
+          <div class="item"><h4>3</h4></div>
+          <div class="item"><h4>4</h4></div>
+          <div class="item"><h4>5</h4></div>
+          <div class="item"><h4>6</h4></div>
+          <div class="item"><h4>7</h4></div>
+          <div class="item"><h4>8</h4></div>
+          <div class="item"><h4>9</h4></div>
+          <div class="item"><h4>10</h4></div>
+          <div class="item"><h4>11</h4></div>
+          <div class="item"><h4>12</h4></div>
+      </div>
       <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+      <script src="{{ mix('js/app.js') }}"></script>
 
 </body>
 </html>
